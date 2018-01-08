832e994b63e074bd7234d5d9d48fdc5d96dbbeaf
Erwin Russel <erwin@wlan-047033.eduroam.tue.nl>
2018-01-08 12:33:55 +0100

    Frontend hierarchy
    Created the frontend hierarchy with a header and a main body

---
+++ neo2pets/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Neo^2 pets Website!</h1>
+  </div>
+)
+
+export default Home
---
+++ neo2pets/src/components/Marketplace.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Marketplace = () => (
+  <div>
+    <h1>Here we can trade on the marketplace</h1>
+  </div>
+)
+
+export default Marketplace
