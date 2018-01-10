3aabcbd1313adc6fc48e7cb5a6e82d0f57884bdb
Rogier96 <rogiertax@hotmail.com>
2018-01-10 14:43:12 +0100

    updated avatarpage

--- neo2pets/src/components/Avatar.js
+++ neo2pets/src/components/Avatar.js
@@ -11,16 +11,17 @@ class Avatar extends Component {
            <div className="block">
              <img className="avatarimage" src={Default}/>
              <div className="avatartext">
-             <p>Pet state: Happy</p>
-             <p>Pet food: not hungry</p>
+             <p>Pet state:</p>
+             Happiness <ProgressBar bsStyle="success" className="progressbar" active now={70} />
+             <p>feed your pet and play games to make it even happier!</p>
              </div>
            </div>
          </Col>
          <Col xs={12} sm={12} md={8}>
           <div className="block jumbotron-style">
+            <div className="padding">
             <h2>Skills</h2>
             <p> improve your skills by playing games! </p>
-            <div >
             speed<ProgressBar className="progressbar" active now={50} />
             agility<ProgressBar className="progressbar" active now={40} />
             Intelligence<ProgressBar className="progressbar" active now={70} />
@@ -29,9 +30,10 @@ class Avatar extends Component {
           </div>
          </Col>
          <Col xs={12} sm={12} md={8}>
-          <div className="block jumbotron-style">
+          <div className="padding block jumbotron-style">
             <h2> Inventory </h2>
-            <Button href="./inventory" jumbotron-style>Go to inventory</Button>
+            Check your items, or go to the shop and buy more!
+            <Button className="padding margin" href="./inventory">Go to inventory</Button>
           </div>
          </Col>
        </Grid>
