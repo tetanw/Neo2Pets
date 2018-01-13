dcdeebfcb13919d3e76cd3910cff0a247733c25a
tetanw <stefan98.willigers@gmail.com>
2018-01-13 18:08:12 +0100

    Centered progress bar

--- neo2pets/src/components/layout/Pet.js
+++ neo2pets/src/components/layout/Pet.js
@@ -1,5 +1,12 @@
-import React, {Component} from 'react';
-import { Button, Grid, Row, Col, Thumbnail, ProgressBar } from 'react-bootstrap';
+import React, { Component } from "react";
+import {
+  Button,
+  Grid,
+  Row,
+  Col,
+  Thumbnail,
+  ProgressBar
+} from "react-bootstrap";
 
 class Pet extends Component {
   render() {
@@ -10,11 +17,19 @@ class Pet extends Component {
             <h3>{this.props.pet.nickName}</h3>
             <h3>{this.props.pet.race.name}</h3>
             <h3>{this.props.pet.name}</h3>
-            <ProgressBar bsStyle="success" className="progressbar" active now={50} />
+            <ProgressBar
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              bsStyle="success"
+              className="progressbar"
+              active
+              now={50}
+            />
           </div>
-        ) : ""}
+        ) : (
+          ""
+        )}
       </div>
-    )
+    );
   }
 }
 
