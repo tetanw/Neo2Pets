50e96c98ee576eae524ce7c83bbbce81826f6cc2
AsciiBunny <turbeau@asciibunny.com>
2018-01-13 23:52:25 +0100

    Inventory page mostly functional

--- neo2pets/src/components/pages/user_section/Inventory/AddStoreModal.js
+++ neo2pets/src/components/pages/user_section/Inventory/AddStoreModal.js
@@ -1,23 +1,73 @@
-import React, { Component } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, {Component} from "react";
+import {Modal, Button, Form, FormGroup, FormControl, Col, ControlLabel} from "react-bootstrap";
 
 class AddStoreModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      price: 0,
+    }
+  }
+
   render() {
-    const { onAddStoreClose, show } = this.props;
+    const {
+      show = false,
+      item,
+      onAddStoreClose,
+    } = this.props;
+
+    if (item === null)
+      return null;
 
     return (
       <Modal show={show} onHide={onAddStoreClose}>
         <Modal.Header closeButton>
-          I just want to say "Add to store"!
+          <Modal.Title>Adding {item.type.name} to your personal store.</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>Add to store!</Modal.Body>
+        <Form onSubmit={this.onSubmit}>
+          <Modal.Body>
+            <FormGroup controlId="formHorizontalPasswordValidation">
+              <Col componentClass={ControlLabel} sm={3}>
+                Item Price
+              </Col>
 
-        <Modal.Footer>
-          <Button> Add to store! </Button>
-        </Modal.Footer>
+              <Col sm={5}>
+                <FormControl
+                  label='Set item price'
+                  className="inputbox jumbotron-style"
+                  type="number"
+                  name="price"
+                  value={this.state.price}
+                  placeholder="100"
+                  onKeyPress={this.onKeyPress}
+                  onChange={(e) => this.setState({price: e.target.value})}
+                  min={0}
+                  required
+                />
+              </Col>
+            </FormGroup>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button type="submit"> Add to store! </Button>
+            <Button onClick={onAddStoreClose}> Close </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     );
+  }
+
+  onSubmit = () => {
+    this.props.onAddStoreClick(this.state.price)
+  }
+
+  onKeyPress = (event) => {
+    const keyCode = event.keyCode || event.which;
+    const keyValue = String.fromCharCode(keyCode);
+    if (/^([^0-9]*)$/.test(keyValue))
+      event.preventDefault();
   }
 }
 
--- neo2pets/src/components/pages/user_section/Inventory/ItemModal.js
+++ neo2pets/src/components/pages/user_section/Inventory/ItemModal.js
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Image } from "react-bootstrap";
+import icon from "../../../../assets/images/neopets/Neopet1.png";
 
 class ItemModal extends Component {
   render() {
     const {
       show = false,
+      item,
       onClose,
       onUseClick,
-      onAddStoreClick,
-      onDeleteClick,
-      onFavouriteClick
+      onAddStoreOpenClick,
+      onDeleteClick
     } = this.props;
+
+    if (item === null)
+      return null;
 
     return (
       <Modal show={show} onHide={onClose}>
-        <Modal.Header closeButton>I just want to say Trello!</Modal.Header>
+        <Modal.Header closeButton>
+          <Modal.Title>{item.type.name}</Modal.Title>
+          </Modal.Header>
 
-        <Modal.Body>Trello!</Modal.Body>
+        <Modal.Body>
+          <Image className=" img-responsive center-block" src={icon} />
+        </Modal.Body>
 
         <Modal.Footer>
-          <Button onClick={onUseClick}> Use </Button>
-          <Button onClick={onAddStoreClick}> Add to store </Button>
-          <Button onClick={onDeleteClick}> Delete </Button>
-          <Button onClick={onFavouriteClick}> Make favourite </Button>
+          <Button onClick={() => onUseClick(item)}> Give to pet </Button>
+          <Button onClick={onAddStoreOpenClick}> Add to store </Button>
+          <Button onClick={() => onDeleteClick(item)}> Throw away </Button>
+          <Button onClick={onClose}> Close </Button>
         </Modal.Footer>
       </Modal>
     );
