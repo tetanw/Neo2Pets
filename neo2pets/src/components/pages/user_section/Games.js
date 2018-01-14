6a91b824280d482be137ea5019ebb67ec7895077
ErwinRussel <ervvin@live.nl>
2018-01-14 21:33:25 +0100

    Fixed gamepanel

--- neo2pets/src/components/pages/user_section/Games.js
+++ neo2pets/src/components/pages/user_section/Games.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import { Link } from "react-router-dom";
+import { Panel, Col, Image, Button } from "react-bootstrap";
+import icon from "../../../assets/images/Snakethumbnail.png"
 
 class Games extends Component {
   render() {
     return (
-      <div className="jumbotron jumbotron-style">
-        <h1>Games</h1>
+      <Panel className="jumbotron-style">
+         <Panel.Heading>
+            <Panel.Title className = "titleinv">Games</Panel.Title>
+          </Panel.Heading>
 
-        <h2>Choose games</h2>
-
-        <h2><Link to='/games/snake'>Snake</Link></h2>      
-
-      </div>
+          <Panel.Body>
+            <Col xs={6} sm={4} md={3} lg={3}>
+            <Link to='/games/snake'>
+            <Panel className= "bgc">
+            <Panel.Body>
+            <Image className="img-responsive center-block" src={icon} />
+            </Panel.Body>
+            <Panel.Footer className="bgc">
+            <div style={{ textAlign: "center" }}>
+              <b> Snake </b>
+            </div>
+             </Panel.Footer>
+            </Panel>
+            </Link>
+            </Col>
+          
+          </Panel.Body>
+      </Panel>
     )
   }
 }
