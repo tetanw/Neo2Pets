3639251e679a4533ad87821841579630c97cdf79
AsciiBunny <turbeau@asciibunny.com>
2018-01-14 13:25:16 +0100

    Create-Avatar Page Redirect

--- neo2pets/src/components/UserSection.js
+++ neo2pets/src/components/UserSection.js
@@ -1,8 +1,10 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
+import {Redirect, Route, Switch} from "react-router";
 import Page from "./UserSectionPage";
 import Pet from "./layout/Pet";
 import CustomNavbar from "./layout/Navbar";
 import {Grid, Col, Row} from "react-bootstrap";
+import CreateAvatar from "./pages/CreateAvatar";
 
 class UserSection extends Component {
   constructor(props) {
@@ -12,22 +14,67 @@ class UserSection extends Component {
     this.updatePet = this.updatePet.bind(this);
 
     this.state = {
-      loading: false
+      loadingPet: false,
+      loadedPet: false,
+      loadingMoney: false,
     };
   }
 
   componentDidMount() {
-    if (!this.state.loading) {
-      this.setState({loading: true});
+    if (!this.state.loadingPet) {
       this.updatePet();
+    }
+
+    if (!this.state.loadingMoney) {
+      this.updateMoney();
     }
   }
 
-  onPetChange() {
+  render() {
+    return (
+      <div>
+        <div>
+          <CustomNavbar/>
+        </div>
+        <Switch>
+          <Route
+            path="/create-avatar"
+            render={props => <CreateAvatar {...props} token={this.props.token}/>}
+          />
+          <Route
+            path="/"
+            render={() =>
+              this.state.loadedPet ?
+                this.state.pet === undefined ? (
+                  <Redirect to="/create-avatar"/>
+                ) : (
+                  <Fragment>
+                    <div className="sidebar">
+                      <Pet token={this.props.token} pet={this.state.pet}/>
+                    </div>
+                    <div className="inline">
+                      <Page token={this.props.token} onPetChange={this.onPetChange}/>
+                    </div>
+                  </Fragment>
+                ) : null
+            }/>
+        </Switch>
+
+      </div>
+    );
+  }
+
+
+  onPetChange = () => {
     this.updatePet();
   }
 
-  updatePet() {
+  onMoneyChange = () => {
+    this.updateMoney();
+  }
+
+  updatePet = () => {
+    this.setState({loadingPet: true});
     fetch("/api/pet/get?userToken=" + this.props.token, {
       method: "GET",
       headers: {
@@ -44,26 +91,35 @@ class UserSection extends Component {
             pet: res.pet,
           });
         }
+        this.setState({
+          loadingPet: false,
+          loadedPet: true,
+        });
       });
   }
 
-  render() {
-    return (
-      <div>
-        <div>
-          <CustomNavbar/>
-        </div>
-        <div>
-          <div className="sidebar">
-            <Pet token={this.props.token} pet={this.state.pet}/>
-          </div>
-          <div className="inline">
-            <Page token={this.props.token} onPetChange={this.onPetChange}/>
-          </div>
-        </div>
-      </div>
-    );
+  updateMoney = () => {
+    this.setState({loadingMoney: true});
+    fetch("/api/money/get?userToken=" + this.props.token, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        console.log(res);
+        if (res.status === "SUCCESS") {
+          this.setState({
+            money: res.money,
+            loadingMoney: false,
+          });
+        }
+      });
   }
+
 }
 
 export default UserSection;
