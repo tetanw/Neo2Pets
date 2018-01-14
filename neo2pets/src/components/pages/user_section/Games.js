a468003fa9e54eba1c2219630efee309341216d5
ErwinRussel <ervvin@live.nl>
2018-01-14 14:41:24 +0100

    Added Game Folders

--- neo2pets/src/components/pages/user_section/Games.js
+++ neo2pets/src/components/pages/user_section/Games.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import Gamechoice from './GamesFolder/Gamechoice';
+import Snake from './GamesFolder/Snake';
+import Minesweeper from './GamesFolder/Minesweeper';
 
 class Games extends Component {
   render() {
     return (
       <div className="jumbotron jumbotron-style">
         <h1>Games</h1>
-        <p>Play games to train your avatar!</p>
+
+        <Switch>
+           <Route
+            exact path="/games/" component={Gamechoice}
+          />
+
+          <Route
+            path="/games/snake" component={Snake}
+          />
+          <Route
+            path="/games/minesweeper" component={Minesweeper}
+          />
+        </Switch>
+
       </div>
     )
   }
 }
+
 
 export default Games
---
+++ neo2pets/src/components/pages/user_section/GamesFolder/Minesweeper.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Minesweeper = () => (
+  <div>
+    <h2>Minesweeper</h2>
+
+    <p> insert game </p>
+
+      <nav>
+            <h2><li><Link to='/games'>Back</Link></li></h2>
+      </nav>
+  </div>
+)
+
+export default Minesweeper
